06ed6fda88c524fc1c8d4494cd3cefbc71e0a88c
Shabhu24@$ <shashipsb.08@gmail.com>
2025-03-06 17:44:35 +0530

    2nd commit

---
+++ src/JavaCourse/HelloWorld.java
@@ -0,0 +1,8 @@
+package JavaCourse;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World, Im going to learn Selenium this time ");
+        System.out.println("Please, bless me god");
+    }
+}
