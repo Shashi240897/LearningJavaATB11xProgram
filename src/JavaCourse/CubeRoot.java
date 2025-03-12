0d3f8d0f4c3b0096ea5fc2730000f056a47477fa
Shabhu24@$ <shashipsb.08@gmail.com>
2025-03-12 17:08:45 +0530

    12th march

---
+++ src/JavaCourse/CubeRoot.java
@@ -0,0 +1,9 @@
+package JavaCourse;
+import java.util.*;
+public class CubeRoot {
+
+    public static void main(String[] args) {
+        int x=10, y=10, z=10;
+        System.out.println(Math.cbrt(Math.pow(10,10)+Math.pow(10,10)-Math.abs(z)));
+    }
+}
---
+++ src/JavaCourse/Final_var.java
@@ -0,0 +1,11 @@
+package JavaCourse;
+
+public class Final_var {
+
+    public static void main(String[] args) {
+        final int a=10;
+        System.out.println(a);
+        //a=20;
+
+    }
+}
