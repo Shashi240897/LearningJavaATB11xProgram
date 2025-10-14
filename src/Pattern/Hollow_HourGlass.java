4f2b819663d5307d5e974e5185a6e31f9eafb11c
Shabhu24@$ <shashipsb.08@gmail.com>
2025-10-14 21:34:04 +0530

    14th oct

--- src/Pattern/Hollow_HourGlass.java
+++ src/Pattern/Hollow_HourGlass.java
@@ -1,47 +1,52 @@
 package Pattern;
 
-public class Hollow_Diamond_Pyramid {
+public class Hollow_HourGlass {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 6; i++){
+        for (int i = 5; i >= 1; i--){
 
-            for (int j = 6; j > i; j--){
+            for (int j = 5; j > i; j--){
                 System.out.print(" ");
 
             }
 
             for (int k = 1; k <= 2 * i - 1; k++){
 
-
+                if(i != 5) {
                     if (k == 1 || k == 2 * i - 1) {
                         System.out.print("*");
                     }
                     else {
                         System.out.print(" ");
                     }
-
+                } else if (k <= i)  {
+                    System.out.print("* ");
+                }
 
             }
             System.out.println();
         }
 
-        for (int i = 5; i >= 1; i--){
+        for (int i = 2; i <= 5; i++){
 
-            for (int j = 6; j > i; j--){
+            for (int j = 5; j > i; j--){
                 System.out.print(" ");
 
             }
 
             for (int k = 1; k <= 2 * i - 1; k++){
 
-
-                    if (k == 1 || k ==  2 * i - 1 ) {
+                if(i != 5) {
+                    if (k == 1 || k == 2 * i - 1) {
                         System.out.print("*");
                     }
                     else {
                         System.out.print(" ");
                     }
+                } else if (k <= i)  {
+                    System.out.print("* ");
+                }
 
             }
             System.out.println();
@@ -49,6 +54,3 @@ public class Hollow_Diamond_Pyramid {
 
     }
 }
-
-
-
